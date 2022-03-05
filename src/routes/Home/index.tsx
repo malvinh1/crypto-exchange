b0ca93d3f23bbb3252b2afdabb11847c9bf8502c
malvinh1 <malvinhariyanto50@gmail.com>
2022-03-05 12:01:46 +0700

    chore: integrate tailwind css

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,5 +1,14 @@
+import BinanceLogo from '../../assets/binance.svg';
+
 const Home = () => {
-  return <div>This is the first page</div>;
+  return (
+    <div>
+      <div className="bg-gray-800">
+        <img src={BinanceLogo} width={120} alt="logo" />
+      </div>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+    </div>
+  );
 };
 
 export default Home;
