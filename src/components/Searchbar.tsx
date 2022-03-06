fc276fffe9fdde3e7af5ba178ebe7c4b44700c11
malvinh1 <malvinhariyanto50@gmail.com>
2022-03-06 16:00:48 +0700

    chore: add alt for images

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -8,7 +8,7 @@ type Props = {
 const Searchbar = ({ value, onChangeValue }: Props) => {
   return (
     <div className="my-4 flex w-fit rounded-md border p-2 hover:border-primary">
-      <img src={SearchIcon} width={24} />
+      <img src={SearchIcon} width={24} alt="search-icon" />
       <input
         className="ml-2 outline-none md:w-64"
         defaultValue={value}
