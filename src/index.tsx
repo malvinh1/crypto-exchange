28a2d429178c8b87d9b6d0a60ac69512c3f1d1c9
malvinh1 <malvinhariyanto50@gmail.com>
2022-03-07 12:55:58 +0700

    chore: add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,3 +12,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
